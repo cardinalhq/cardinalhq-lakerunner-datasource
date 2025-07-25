85fbfd7e87d5a495ca5c35ec15a50861e014baab
Kunal Kundaje <kunal@cardinalhq.io>
2025-07-24 17:34:24 -0700

    Update build script to also sign the plugin

--- build.sh
+++ build.sh
@@ -10,6 +10,8 @@ rm -rf cardinalhq-lakerunner-datasource*.zip
 npm run build
 mage
 
+npx @grafana/sign-plugin@latest --rootUrls=http://localhost:3000,https://grafana.cardinalhq.io
+
 mv dist cardinalhq-lakerunner-datasource
 zip -r cardinalhq-lakerunner-datasource.zip cardinalhq-lakerunner-datasource
 
